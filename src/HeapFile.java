7c509df912b7d029e91e5f07290eb9816a8d7e5f
jeffreycd <jeffreycd@qq.com>
2017-05-25 19:40:19 +0800

    fix bug: the last write was ignored

--- src/HeapFile.java
+++ src/HeapFile.java
@@ -18,20 +18,27 @@ public class HeapFile {
             BufferedReader reader = new BufferedReader(new FileReader(path));//换成你的文件名
             reader.readLine();//第一行信息，为标题信息，不用，如果需要，注释掉
             String line;
-            long count = 1;
+            long count = 0;
             int size = HeapPage.size / Record.bytes();
-            HeapPage page = new HeapPage((int)(count / size));
+            HeapPage page = null;
             while((line = reader.readLine())!= null){
                 int index = (int) count % size;
                 if (index == 0) {
-                    page.writeToFile(dir);
-                    System.out.println("write page No." + count / size);
+                    if (count != 0) {
+                        page.writeToFile(dir, size);
+                        System.out.println("write page No." + count / size);
+                    }
                     page = new HeapPage((int)(count / size));
                 }
 //                System.out.println(line);
                 String item[] = line.split(",");//CSV格式文件为逗号分隔符文件，这里根据逗号切分
                 page.addRecord(index, item);
                 count++;
+            }
+            int remainder = (int)((count-1) % size);
+            if (remainder != 0) {
+                page.writeToFile(dir, remainder);
+                System.out.println("write page No." + count / size);
             }
         } catch (Exception e) {
             e.printStackTrace();
@@ -62,12 +69,12 @@ class HeapPage {
         return pageID;
     }
 
-    public void writeToFile(String dir) {
+    public void writeToFile(String dir, int len) {
         File file = new File(dir + "/" + pageID + ".page");
         try {
             createFile(file, size);
             BufferedOutputStream out = new BufferedOutputStream(new FileOutputStream(file));
-            for (int i = 0; i < records.length; i++) {
+            for (int i = 0; i < len; i++) {
                 out.write(records[i].toByteStream());
             }
             out.flush();
@@ -162,7 +169,7 @@ class HeapPage {
         for (int i = 0; i < page.records.length; i++) {
             page.records[i] = new Record(i, "2011-08-25", 2009+i,"March", 1, "Monday", 4, 9527, "LuoPing", 103);
         }
-        page.writeToFile("disc");
+        page.writeToFile("disc", page.records.length);
         HeapPage newPage = HeapPage.readFromFile("disc/page/1.page");
         for (int i = 0; i < page.records.length; i++) {
             System.out.println(newPage.records[i]);
